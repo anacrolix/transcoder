6b9694b70a4590d5f5057c76f230ad5cc80bd959
Matt Joiner <anacrolix@gmail.com>
2022-04-15 19:10:39 +1000

    Follow Data.ByteString.Streaming deprecation recommendations

--- Extra.hs
+++ Extra.hs
@@ -1,15 +1,14 @@
 module Extra where
 
-import           Control.Monad.Trans                (MonadIO, liftIO)
-import           Control.Monad.Trans.Resource       (MonadResource)
-import           Data.ByteString.Streaming          (ByteString, hGetContents)
-import           Data.ByteString.Streaming.Internal (bracketByteString)
-import           System.IO                          (IOMode (ReadMode),
-                                                     SeekMode (AbsoluteSeek),
-                                                     hClose, hSeek,
-                                                     openBinaryFile)
+import           Control.Monad.Trans           (MonadIO, liftIO)
+import           Control.Monad.Trans.Resource  (MonadResource)
+import           Streaming.ByteString          (ByteStream, hGetContents)
+import           Streaming.ByteString.Internal (bracketByteString)
+import           System.IO                     (IOMode (ReadMode),
+                                                SeekMode (AbsoluteSeek), hClose,
+                                                hSeek, openBinaryFile)
 
-readFileFrom :: (MonadResource m, MonadIO m) => Integer -> FilePath -> ByteString m ()
+readFileFrom :: (MonadResource m, MonadIO m) => Integer -> FilePath -> ByteStream m ()
 readFileFrom off path = bracketByteString (openBinaryFile path ReadMode) hClose $ \h -> do
     liftIO $ hSeek h AbsoluteSeek off
     hGetContents h
